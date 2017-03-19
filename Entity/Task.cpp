5a39c110e8e110c92532db427059913eb6bab8ce
oshipka <olenkashipka@gmail.com>
2017-03-19 13:10:03 +0200

    Update Task.cpp

--- Entity/Task.cpp
+++ Entity/Task.cpp
@@ -46,13 +46,3 @@ istream& operator >> (istream& is, Task &task) //overload operator
      task.set_state(state);
      return is;
 }
-
-void Task::show_all_task()   //should be in Menu.cpp
-{
-    vector<Task> tasks(DTOTask::write_from_file());
-    cout << "Name: " << "\t" << "State: " << endl;
-    for(int i = 0 ; i < tasks.size(); i++)
-    {
-        cout << tasks[i].get_name() << "\t" << tasks[i].get_state() << endl;
-    }
-}
