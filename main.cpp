27d9b15b4c252e9c53f821cdefa5ffd2f1ae687a
oshipka <olenkashipka@gmail.com>
2017-03-19 12:59:34 +0200

    Updated main

--- main.cpp
+++ main.cpp
@@ -1,16 +1,13 @@
 #include "Entity/Task.h"
 #include "DTO/DTOTask.h"
-#include "BL/Menu.cpp" //there should be changes
+#include "BL/Menu.h" //there should be changes
 
 
 int main()
 {
     DTOTask d;
     Task tasks;
-    tasks.set_name ("Name_temp");
-    tasks.set_state ("State_temp");
-    d.write_to_file(tasks);
-    tasks.show_all_task();
+	menu(d, tasks);
     return 0;
 }
 
